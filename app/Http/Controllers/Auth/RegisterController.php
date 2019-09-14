3111c3bf157e9962f61a74b40317af76b667e661
jgbeltran <jerik@courtesypoint.com>
2019-09-15 03:15:17 +0700

    Update Registration Page
    - FullName, UserName and Password is required upon registration.  Registration with user group is not yet implemented.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,8 +49,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'fullName' => ['required', 'string', 'max:100'],
+            'username' => ['required', 'string', 'max:50', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -64,8 +64,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
+            'full_name' => $data['fullName'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
         ]);
     }
